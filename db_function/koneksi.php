d9a69d0060b04b132422631531727dfe79062f34
King Zuy <zulfifathoni03@gmail.com>
2023-01-05 10:45:28 +0700

    first

---
+++ db_function/koneksi.php
@@ -0,0 +1,10 @@
+<?php
+
+$ENV = parse_ini_file('../.env')['DB_PASSWORD'];
+
+define("HOST", $ENV['DB_HOST']);
+define("USER", $ENV['DB_USERNAME']);
+define("PASS", $ENV['DB_PASSWORD']);
+define("DB", $ENV['DB_DATABASE']);
+
+$db = new mysqli(HOST, USER, PASS, DB) or die("Couldn't connect to database");
