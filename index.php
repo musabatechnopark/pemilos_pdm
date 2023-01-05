3c32928d3c8fa6be148fe652c0a3068f547d1f35
King Zuy <zulfifathoni03@gmail.com>
2023-01-05 11:31:32 +0700

    new

--- index.php
+++ index.php
@@ -1,1 +1,8 @@
-<?= "hallo"; ?>
+<?php
+if (isset($_SESSION['username'])) :
+    header("location:javascript://history.go(-1)");
+elseif (empty($_GET['login'])) :
+    include_once('login.php');
+else :
+    include_once('admin_login.php');
+endif;
---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<style>
+    /* Chrome, Safari, Edge, Opera */
+    input::-webkit-outer-spin-button,
+    input::-webkit-inner-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
+
+    /* Firefox */
+    input[type=number] {
+        -moz-appearance: textfield;
+    }
+</style>
+
+<body>
+    <center>
+        Login <br> <br>
+        nbm : <input type="number" name="nbm" id=""><br><br>
+        pass : <input type="number" name="password" id=""> <br><br>
+        <button type="submit">Login</button>
+    </center>
+</body>
+
+</html>
