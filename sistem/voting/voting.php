9980012fe47ea51b50622709ef9c586ac604c757
King Zuy <zulfifathoni03@gmail.com>
2023-01-24 08:37:34 +0700

    voting sistem

--- sistem/voting/voting.php
+++ sistem/voting/voting.php
@@ -6,5 +6,12 @@ if (empty($_SESSION['login'])) return $_SESSION['error_logut'] = 'You not login'
 switch ($request['voting']) {
     case 'true':
 
+        $nbm = $_SESSION['nbm'];
+        foreach ($request as $key => $value) :
+            if (!empty($value) && is_numeric($value)) :
+                echo $key . ' => ' . $value . "\n";
+            endif;
+        endforeach;
+        echo $nbm;
         break;
 }
