8f67255c3f28388d2146972a07a06086e5164e40
King Zuy <zulfifathoni03@gmail.com>
2023-02-03 13:15:29 +0700

    db

--- database/pemilos (1).sql
+++ database/pemilos (1).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 16, 2023 at 08:33 AM
--- Server version: 8.0.31-0ubuntu0.22.04.1
--- PHP Version: 8.1.2-1ubuntu2.9
+-- Generation Time: Feb 03, 2023 at 01:14 PM
+-- Server version: 8.0.32-0ubuntu0.22.04.2
+-- PHP Version: 8.1.2-1ubuntu2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -48,20 +48,51 @@ INSERT INTO `tb_admin` (`id`, `username`, `password`) VALUES
 
 CREATE TABLE `tb_calon` (
   `id` int NOT NULL,
-  `nbm` char(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `nbm` char(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL,
   `calon_no` varchar(10) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
   `nama` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
-  `data` json NOT NULL,
-  `img` varchar(255) COLLATE utf8mb4_general_ci NOT NULL
+  `data` json DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tb_calon`
 --
 
-INSERT INTO `tb_calon` (`id`, `nbm`, `calon_no`, `nama`, `data`, `img`) VALUES
-(1, '123412341234567', 'CALON 1', 'Aji Saka', '{\"alamat\": \"bantul\", \"pekerjaan\": \"nanggur\", \"pendidikan\": \"smp unggulan aisyiyah bantul\"}', 'aji.jpg'),
-(2, '122212221234567', 'CALON 2', 'Rapli', '{\"alamat\": \"bantul\", \"pekerjaan\": \"nanggur\", \"pendidikan\": \"smp unggulan aisyiyah bantul\"}', 'rapli.jpg');
+INSERT INTO `tb_calon` (`id`, `nbm`, `calon_no`, `nama`, `data`) VALUES
+(1, '646641', 'CALON 1', 'Suwardiman Anwar Huda, S.Ag., M.S.I', NULL),
+(2, '630959', 'CALON 2', 'Encep Komarudin, M.S.I', NULL),
+(3, '559556', 'CALON 3', 'Drs. H. Parjiman, M.Ag', NULL),
+(4, '933155', 'CALON 4', 'H. Muhammad Nur Amin Shalashani, S.H.I', NULL),
+(5, '649778', 'CALON 5', 'H. Sukirman, SH', NULL),
+(6, '790217', 'CALON 6', 'Drs. Supriyadi, MM', NULL),
+(7, '656429', 'CALON 7', 'Basrodin, M.Pd', NULL),
+(8, '664544', 'CALON 8', 'Arba Riksawan Qomaru, S.E', NULL),
+(9, '787395', 'CALON 9', 'Aris Samsugito, S.Ag', NULL),
+(10, '506282', 'CALON 10', 'Drs. H. Saebani, MA, M.Pd', NULL),
+(11, '625552', 'CALON 11', 'Agus Mulyono', NULL),
+(12, '663369', 'CALON 12', 'Drs. Hendarto, MA', NULL),
+(13, '793193', 'CALON 13', 'H. Jiriban, SH, CN', NULL),
+(14, '992334', 'CALON 14', 'Muhammad Farid Hadiyanto, SE', NULL),
+(15, '0', ' CALON15', 'Suwarjono', NULL),
+(16, '975963', 'CALON 16', 'Ardi Kiswanto', NULL),
+(17, '1070156', 'CALON 17', 'Yahya Hanafi, M.Sc', NULL),
+(18, '630737', 'CALON 18', 'Sugeng Prihatin, SH', NULL),
+(19, '0', 'CALON 19', 'H. Suhartadi Prasojo, S.E', NULL),
+(20, '0', 'CALON 20', 'Suwandi Danu Subroto', NULL),
+(21, '0', 'CALON 21', 'Drs. H. Agus Amarullah, MA', NULL),
+(22, '0', 'CALON 22', 'Drs. H. Sahari', NULL),
+(23, '0', 'CALON 23', 'Drs. H. Bambang inanta', NULL),
+(24, '918183', 'CALON 24', 'Abdul Haris, SIP, MEK', NULL),
+(25, '627757', 'CALON 25', 'Dr. Dedi Pramono, M.Hum.', NULL),
+(26, '600379', 'CALON 26', 'Toto Budi Santosa', NULL),
+(27, '549325', 'CALON 27', 'Drs. Kun Purwanto', NULL),
+(28, '556904', 'CALON 28', 'Drs. Purwana, MA', NULL),
+(29, '480218', 'CALON 29', 'Drs. H. Sunarto', NULL),
+(30, '661368', 'CALON 30', 'Drs. Mugiyanto, MSI', NULL),
+(31, '913370', 'CALON 31', 'Agus Budiantoro, S.IP, S.AP', NULL),
+(32, '611749', 'CALON 32', 'Drs. Totok Sudarto, M.Pd', NULL),
+(33, '376262', 'CALON 33', 'Drs. M. Syahro Hadiputro', NULL),
+(34, '664547', 'CALON 34', 'Farida Ulfah Makrifah', NULL);
 
 -- --------------------------------------------------------
 
@@ -80,7 +111,8 @@ CREATE TABLE `tb_login` (
 --
 
 INSERT INTO `tb_login` (`id`, `nbm`, `password`) VALUES
-(5, '123412341234567', '7c222fb2927d828af22f592134e8932480637c0d');
+(5, '123412341234567', '7c222fb2927d828af22f592134e8932480637c0d'),
+(6, '1234', '7c222fb2927d828af22f592134e8932480637c0d');
 
 -- --------------------------------------------------------
 
@@ -101,7 +133,9 @@ CREATE TABLE `tb_pengguna` (
 --
 
 INSERT INTO `tb_pengguna` (`id`, `nbm`, `nama`, `cabang`, `pemilos`) VALUES
-(125, '123412341234567', 'King Zuy', 'Kretek', 'belum');
+(125, '123412341234567', 'King Zuy', 'Kretek', 'belum'),
+(126, '12312412312', 'Suka Bapa', 'Bambang', 'belum'),
+(127, '1234', 'Sima Sukima', 'Purwodadi', 'belum');
 
 -- --------------------------------------------------------
 
@@ -115,13 +149,6 @@ CREATE TABLE `tb_suara` (
   `calon_id` int NOT NULL,
   `jumlah` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `tb_suara`
---
-
-INSERT INTO `tb_suara` (`id`, `pengguna_id`, `calon_id`, `jumlah`) VALUES
-(1, 125, 1, 2);
 
 --
 -- Indexes for dumped tables
@@ -177,25 +204,25 @@ ALTER TABLE `tb_admin`
 -- AUTO_INCREMENT for table `tb_calon`
 --
 ALTER TABLE `tb_calon`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT for table `tb_login`
 --
 ALTER TABLE `tb_login`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tb_pengguna`
 --
 ALTER TABLE `tb_pengguna`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=126;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=128;
 
 --
 -- AUTO_INCREMENT for table `tb_suara`
 --
 ALTER TABLE `tb_suara`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
 --
 -- Constraints for dumped tables
