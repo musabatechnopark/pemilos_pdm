38d2759ba9a72bf000e35cad294eea19ce260e10
King Zuy <74240904+kingzuy@users.noreply.github.com>
2023-02-23 15:50:55 +0700

    Revert "Muhammadiyah"

--- countdown.php
+++ countdown.php
@@ -15,9 +15,7 @@ if ($data->pemilos == 1) return return_url('index.php');
 <html>
 
 <head>
-  <link rel="shortcut icon" href="assets/img/techno.png" type="image/x-icon">
-
-  <title>Countdown</title>
+  <title>E-Voting</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="assets/css/user.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
